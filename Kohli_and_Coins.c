ef6e451c1e147ffd2aeb889f1fc5fb86ade0763f
Srinadh2026 <119730199+Srinadh2026@users.noreply.github.com>
2022-12-24 19:25:12 +0530

    #include<stdio.h> int main() {     int n,a;     scanf("%d",&n);     if(n%10==5 || n%10==0)     {         if(n%10==0)         printf("%d",n/10);         else if(n%10==5) printf("%d",n/10+1); } else {     printf("-1");
    }
        
    }

---
+++ Kohli_and_Coins.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{ 
+    int n,a;
+    scanf("%d",&n);
+    if(n%10==5 || n%10==0) 
+    {
+        if(n%10==0)
+        printf("%d",n/10);
+        else if(n%10==5)
+printf("%d",n/10+1);
+}
+else 
+{
+    printf("-1"); 
+    
+}
+    
+}
