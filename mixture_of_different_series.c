bd260dd88e51de30f0864b36874a93f200ee2ca8
Srinadh2026 <119730199+Srinadh2026@users.noreply.github.com>
2023-01-04 22:29:12 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i,s,a,k1=0,k2=0;     scanf("%d",&n);     for(i=1;i<=n+1;i++)     {         if(i%2!=0)         {             s=pow(2,k1)-1;             printf("%d ",s); k1++; } else {     a=pow(3,k2)-1;     printf("%d ",a);     k2++;
    }
    }
        
    }

---
+++ mixture_of_different_series.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,s,a,k1=0,k2=0;
+    scanf("%d",&n);
+    for(i=1;i<=n+1;i++)
+    { 
+        if(i%2!=0) 
+        { 
+            s=pow(2,k1)-1;
+            printf("%d ",s);
+k1++;
+}
+else
+{
+    a=pow(3,k2)-1;
+    printf("%d ",a);
+    k2++; 
+    
+}
+}
+    
+}
